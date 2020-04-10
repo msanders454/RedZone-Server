876b28033a9941a7fbb43720ebaab9a52a40e17b
Michael Sanders <mikesanderspsu@gmail.com>
2020-04-10 16:34:41 -0400

    Added Api add

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin@localhost/red-zone',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/red-zone',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
